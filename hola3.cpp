1fba1460727814be9083ee87a05a0dcfc112a639
JavierFarrona <alu0101541983@ull.edu.es>
2023-09-16 16:09:34 +0100

    Primer comit

---
+++ hola3.cpp
@@ -0,0 +1,9 @@
+//haz un codigo en c++ para un Hello world
+#include <iostream> //incluye el header de iostream, que contiene la funcion cout y cin.
+int main() {
+    std::cout << "Hello World" << std::endl;
+    // todo: Return
+    // !
+    // ?
+    // * 
+}
